88af37adba22cdac3b208615f719712aaaee2332
Amine <am1ne.maalouf7@gmail.com>
2022-03-05 18:37:53 +0100

    Working demo

--- Unity_S2/Assets/Matchmaking/Launcher.cs
+++ Unity_S2/Assets/Matchmaking/Launcher.cs
@@ -65,7 +65,7 @@ namespace Matchmaking
         private bool switchingscene;
         private void Update()
         {
-            if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2 && !switchingscene)
+            if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 1 && !switchingscene)
             {
                 switchingscene = true;
                 PhotonNetwork.LoadLevel(2);
