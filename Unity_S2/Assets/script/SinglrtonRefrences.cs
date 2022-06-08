21712f3a2fe6a6ba6d8a34a5b657248f2fa49a5b
tina <christina.lopes92@gmail.com>
2022-06-08 13:13:40 +0200

    message commit

--- Unity_S2/Assets/script/SinglrtonRefrences.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SinglrtonRefrences : MonoBehaviour
-{
-    [SerializeField] 
-    private MasterManager _masterManager;
-}
